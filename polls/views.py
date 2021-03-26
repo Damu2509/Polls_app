85240bcff690e5307ba047d60f424caec1b45033
Damu2509 <reddivaridamu25091999@gmail.com>
2021-03-26 22:33:33 +0530

    Modified urls and views

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,19 @@ def index(request):
     return HttpResponse(" <h1>hii there ! Your django app is ready to go , bhoom! </h1>")
 
 
+def detail(request, question_id):
+    return HttpResponse(" <h1> You are looking at question %s. </h1>" %question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s ."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(" <h1> you are voting on %s . </h1>" %question_id)
+
+
 
 
 # Create your views here.
