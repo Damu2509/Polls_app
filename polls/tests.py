2cd3c9b6e3840971b51e712b66e72872b29c6097
Damu2509 <reddivaridamu25091999@gmail.com>
2021-03-29 23:58:22 +0530

    Added some more test cases

--- polls/tests.py
+++ polls/tests.py
@@ -3,8 +3,13 @@ from django.test import TestCase
 import datetime
 from django.utils import timezone
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Question
+
+def create_question(questio_text, days):
+    time = timezone.now() + datetime.timedelta(days = days)
+    return Question.objects.create(question_text = question_text, pub_date = time)
 
 class QuestionModelTest(TestCase):
 
@@ -27,4 +32,23 @@ def test_was_published_recently_with_recent_question(self):
     time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
     recent_question = Question(pub_date = time)
     self.assertIs(recent_question.was_published_recently(), True)
+
+
+class QuestionIndexviewTests(TestCase):
+
+    def test_no_questions(self):
+
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,  " No polls are available")
+        self.assertQuerysetEqual(response.context['latest_question_list'],[])
+
+    def test_past_questions(self):
+
+        create_question(question_text = "Refers to past question.", days=-30)
+        resposne =  self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<qustion:Refers to past question.>'])
+
+        
+
 
