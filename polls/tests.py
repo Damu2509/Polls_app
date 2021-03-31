b27fa6a673f8edb1ca9e0424f9079828646a9752
Damu2509 <reddivaridamu25091999@gmail.com>
2021-03-31 23:36:04 +0530

    Added a test case

--- polls/tests.py
+++ polls/tests.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 
 from .models import Question
 
-def create_question(questio_text, days):
+def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days = days)
     return Question.objects.create(question_text = question_text, pub_date = time)
 
@@ -36,18 +36,27 @@ def test_was_published_recently_with_recent_question(self):
 
 class QuestionIndexviewTests(TestCase):
 
-    def test_no_questions(self):
+    def test_no_question(self):
 
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code,200)
         self.assertContains(response,  " No polls are available")
         self.assertQuerysetEqual(response.context['latest_question_list'],[])
 
-    def test_past_questions(self):
+    def test_past_question(self):
 
-        create_question(question_text = "Refers to past question.", days=-30)
-        resposne =  self.client.get(reverse('polls:index'))
-        self.assertQuerysetEqual(response.context['latest_question_list'], ['<qustion:Refers to past question.>'])
+        create_question(question_text ="Refers to past question.", days=-30)
+        response =  self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question:Refers to past question.>'])
+
+    def test_future_question(self):
+
+        create_question(question_text = "Future Question . " , days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context['latest_question_list'],[])
+
+
 
         
 
