52d881723ec4245c71b3bdc25db21fa2f9fc5121
Damu2509 <reddivaridamu25091999@gmail.com>
2021-04-04 21:12:24 +0530

    Added admin and models apps functionalities

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,7 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
@@ -21,5 +22,9 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+was_published_recently.admin_order_field = 'pub_date'
+was_published_recentlly.boolean = True
+was_published_recently.short_description = 'published recently ? '
 
 
