1bde4221fa5b695a168d71bfd7457677fabc696c
Damu2509 <reddivaridamu25091999@gmail.com>
2021-04-08 20:51:00 +0530

    Added admin page functionalities

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,10 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+        was_published_recently.admin_order_field = 'question_text'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Pubished Recently ? '
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
