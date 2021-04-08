f3fd44070cd2423adf4fc82d132d9dedff51d11d
Damu2509 <reddivaridamu25091999@gmail.com>
2021-04-08 08:55:10 +0530

    Working on admin page

--- polls/admin.py
+++ polls/admin.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 from .models import Question,Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
  
-    fieldsets = [("change the question you woud love to", {'fields': [ 'question_text'] }), ('Date information', {'fields': ['pub_date'], 'classes': ['collapse'], })]
+    fieldsets = [("change the question you woud love to", {'fields': [ 'question_text',] }), ('Publication Date information', {'fields': ['pub_date'], 'classes': ['collapse'], })]
     inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date','was_published_recently')
 
 
 
